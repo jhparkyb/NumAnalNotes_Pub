fddd38cbb71c10bbf6ba4d4166899cc44e993397
jhparkyb <jhpofficedrive@gmail.com>
2024-05-03 14:00:34 -0700

    QR algorithm only prelem left

--- chapters/internallib.py
+++ chapters/internallib.py
@@ -186,3 +186,31 @@ def poly_eval_simplest(c, x, order='increasing'):
     pow = np.arange(n) if order == 'increasing' else np.arange(n)[::-1]
 
     return np.sum(c * np.power(x[:, np.newaxis], pow), axis=1)
+
+def qr(A):
+    """
+    Return full QR factorization.
+
+    Input:
+        A (array): a tall rectangular or square matrix
+    Output:
+        Q, R (array): factors Q, R in QR factorization
+    """
+    m, n = A.shape
+    R = A.copy()
+    Q = np.eye(m)
+
+    for i in range(n):
+        k = m - i
+
+        x = R[i:, i].reshape(-1, 1)
+        w = np.zeros_like(x).reshape(-1, 1)
+        w[0] = - np.sign(x[0])*np.linalg.norm(x)
+
+        v = w - x
+        v_ = ((2./(v.T @ v))*v)
+        R[i:, i:] = R[i:, i:] - v_ @ (v.T @ R[i:, i:])
+        
+        Q[:, i:] = Q[:, i:] - (Q[:, i:] @ v_) @ v.T
+
+    return Q, R    
